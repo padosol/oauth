d8946383284bb4818f03f8adbc5fbd607539b590
한상훈 <hzyan2@gmail.com>
2025-06-03 22:13:38 +0900

    feat: SocialOauth 인터페이스 추가

--- src/main/java/com/ecommerce/userservice/user/controller/OauthController.java
+++ src/main/java/com/ecommerce/userservice/user/controller/OauthController.java
@@ -1,19 +1,34 @@
 package com.ecommerce.userservice.user.controller;
 
+import com.ecommerce.userservice.user.service.OauthService;
 import com.ecommerce.userservice.user.util.SocialLoginType;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping("/auth")
 @RestController
+@RequiredArgsConstructor
 public class OauthController {
+
+    private final OauthService oauthService;
 
     @GetMapping("/{socialLoginType}")
     public void socialLoginType(
-            @PathVariable("socialLoginType") SocialLoginType socialLoginType
+            @PathVariable("socialLoginType") SocialLoginType socialLoginType,
+            HttpServletResponse response
     ) {
         log.info("로그인 요청 {}", socialLoginType);
+        String redirectURL = oauthService.createRedirectURL(socialLoginType);
+
+        try {
+            response.sendRedirect(redirectURL);
+        } catch(Exception e) {
+            log.error("Redirect error");
+        }
     }
 
     @GetMapping("/{socialLoginType}/callback")
