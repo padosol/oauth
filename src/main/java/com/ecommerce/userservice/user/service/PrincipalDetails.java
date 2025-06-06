fa2afc8d38fcd34ac860ade077d8b8863e151b17
한상훈 <hzyan2@gmail.com>
2025-06-06 23:59:17 +0900

    feat: princiaplDetails 추가

--- src/main/java/com/ecommerce/userservice/user/service/PrincipalDetails.java
+++ src/main/java/com/ecommerce/userservice/user/service/PrincipalDetails.java
@@ -1,4 +1,35 @@
 package com.ecommerce.userservice.user.service;
 
-public class PrincipalDetails {
+import com.ecommerce.userservice.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public class PrincipalDetails implements OAuth2User {
+
+    private final User user;
+    private final Map<String, Object> attributes;
+
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
 }
